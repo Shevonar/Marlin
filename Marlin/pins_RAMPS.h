b478a3ecf37da07e4f46fd53822ddfca1948f432
Matthias Horne <infomh@anmaruco.de>
2018-04-22 17:55:42 +0200

    Switch to TMC2130

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -91,7 +91,7 @@
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
-#define X_CS_PIN           53
+#define X_CS_PIN           40 // 53
 
 #define Y_STEP_PIN         60
 #define Y_DIR_PIN          61
@@ -101,7 +101,7 @@
 #define Z_STEP_PIN         46
 #define Z_DIR_PIN          48
 #define Z_ENABLE_PIN       62
-#define Z_CS_PIN           40
+#define Z_CS_PIN           53 // 40
 
 #define E0_STEP_PIN        26
 #define E0_DIR_PIN         28
