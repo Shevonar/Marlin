8ecaf245b2e33b54c5f9ebf0e8d7677caa87d7ef
Matthias Horne <infomh@anmaruco.de>
2018-05-02 19:27:13 +0200

    Switch to spreadcycle mode

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1075,13 +1075,13 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          650  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT          650
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          800
+  #define Z_CURRENT          650
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
@@ -1093,7 +1093,7 @@
   #define Z2_CURRENT         800
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         800
+  #define E0_CURRENT         650
   #define E0_MICROSTEPS       16
 
   #define E1_CURRENT         800
@@ -1122,7 +1122,7 @@
    * Use Trinamic's ultra quiet stepping mode.
    * When disabled, Marlin will use spreadCycle stepping mode.
    */
-  #define STEALTHCHOP
+  //#define STEALTHCHOP
 
   /**
    * Monitor Trinamic TMC2130 and TMC2208 drivers for error conditions,
